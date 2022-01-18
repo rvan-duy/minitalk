26568a220e859ea58529349632ec2a728a3791d3
rvan-duy <rvanduyneveldt@gmail.com>
2022-01-18 12:15:16 +0100

    first commit

--- src/client.c
+++ src/client.c
@@ -6,17 +6,45 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/12 12:59:02 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/01/12 13:31:28 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/01/14 16:31:14 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+void	char_to_signal(unsigned char character, pid_t pid)
+{
+	const size_t	len = sizeof(character) * 8;
+	size_t			i;
+
+	i = 8;
+	while (i > 0)
+	{
+		if (character & (1 << i))
+			printf("1");
+		else
+			printf("0");
+		i++;
+	}
+	printf("\n");
+}
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-	printf("Hey\n");
-	return (0);
+	const int	server_pid = ft_atoi(argv[1]);
+	const char	*string_to_send = ft_strdup(argv[2]);
+
+	if (string_to_send == NULL)
+		return (EXIT_FAILURE);
+	if (argc == 3)
+	{
+		kill(server_pid, SIGSTOP);
+		ft_putnbr_fd(server_pid, STDOUT_FILENO);
+		ft_putchar_fd('\n', STDOUT_FILENO);
+		ft_putendl_fd(string_to_send, STDOUT_FILENO);
+	}
+	return (EXIT_SUCCESS);
 }
