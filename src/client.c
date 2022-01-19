a85c5c9b35e442d5fb0ac264cc45140c4f86eccf
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-01-19 17:52:33 +0100

    sending chars now

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/12 12:59:02 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/01/14 16:31:14 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/01/19 17:51:51 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,34 +17,45 @@
 
 void	char_to_signal(unsigned char character, pid_t pid)
 {
-	const size_t	len = sizeof(character) * 8;
-	size_t			i;
+	int	i;
+	int	bit;
 
-	i = 8;
-	while (i > 0)
+	i = 0;
+	printf("sending %c to server...\n", character);
+	while (i < 8)
 	{
-		if (character & (1 << i))
-			printf("1");
+		bit = 1 & character;
+		if (bit == 1)
+			kill(pid, SIGUSR1);
 		else
-			printf("0");
+			kill(pid, SIGUSR2);
+		character = character >> 1;
 		i++;
+		usleep(1000);
 	}
-	printf("\n");
 }
 
 int	main(int argc, char **argv)
 {
-	const int	server_pid = ft_atoi(argv[1]);
-	const char	*string_to_send = ft_strdup(argv[2]);
+	const int		server_pid = ft_atoi(argv[1]);
+	const char		*string_to_send = ft_strdup(argv[2]);
+	const size_t	string_len = ft_strlen(string_to_send);
+	size_t			i;
 
+	i = 0;
 	if (string_to_send == NULL)
 		return (EXIT_FAILURE);
 	if (argc == 3)
 	{
-		kill(server_pid, SIGSTOP);
-		ft_putnbr_fd(server_pid, STDOUT_FILENO);
-		ft_putchar_fd('\n', STDOUT_FILENO);
-		ft_putendl_fd(string_to_send, STDOUT_FILENO);
+		while (i < string_len)
+		{
+			char_to_signal(string_to_send[i], server_pid);
+			i++;
+		}
+		// kill(server_pid, SIGSTOP);
+		// ft_putnbr_fd(server_pid, STDOUT_FILENO);
+		// ft_putchar_fd('\n', STDOUT_FILENO);
+		// ft_putendl_fd(string_to_send, STDOUT_FILENO);
 	}
 	return (EXIT_SUCCESS);
 }
--- src/server.c
+++ src/server.c
@@ -6,16 +6,51 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/12 11:04:45 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2022/01/14 13:55:40 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2022/01/19 17:44:41 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include "server.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
+
+void	signal_handler(int sig)
+{
+	static size_t	current_bit = 1;
+	static char		current_character = 0;
+
+	printf("current_bit = %zu\n", current_bit);
+	if (sig == 30)
+	{
+		printf("received sigusr1 signal: corresponding to bit 1\n");
+		current_character += current_bit;
+	}
+	else if (sig == 31)
+	{
+		printf("received sigusr2 signal: corresponding to bit 0\n");
+	}
+	current_bit *= 2;
+	if (current_bit > 128)
+	{
+		printf("found character: %c\n", current_character);
+		current_bit = 1;
+		current_character = 0;
+	}
+}
 
 int	main(void)
 {
-	printf("%d\n", getpid());
-	while (1);
+	struct sigaction	sa;
+
+	sa.sa_flags = SA_RESTART;
+	sa.sa_handler = &signal_handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putchar_fd('\n', 1);
+	while (1)
+		;
 	return (0);
 }
